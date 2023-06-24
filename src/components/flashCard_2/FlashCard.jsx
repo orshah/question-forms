216f8e1b2c5cc6ba97fdfa493ace94c37179ad25
orshah <orshpov@gmail.com>
2023-06-24 09:18:59 -0500

    aligned flashcards

--- src/components/flashCard_2/FlashCard.jsx
+++ src/components/flashCard_2/FlashCard.jsx
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import "./flashCard.style.css";
 
 function FlashCard() {
   const theme = useTheme();
